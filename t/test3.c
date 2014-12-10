711c8b46f4eef08a9be8b7e9258e594883b9f530
Austin Seipp <aseipp@pobox.com>
2014-12-10 07:48:50 -0600

    Initial commit.
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ t/test3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+void
+h()
+{
+  printf("h(): crashing\n");
+  fflush(stdout);
+
+  assert(false);
+}
+
+void
+g()
+{
+  printf("g(): gonna crash soon\n");
+  h();
+}
+
+void
+f()
+{
+  printf("f(): presumably LD_PRELOADed\n");
+  g();
+}
+
+int
+main(int ac, char** av)
+{
+  f(); /* Go */
+  return 0;
+}
